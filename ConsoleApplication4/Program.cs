0560b245a11d39c663f0ce31c80ba08f1e9cab80
dragonar <fugedi4@gmail.com>
2017-02-23 12:53:20 +0100

    meghivások

--- ConsoleApplication4/Program.cs
+++ ConsoleApplication4/Program.cs
@@ -10,6 +10,16 @@ namespace ConsoleApplication4
     {
         static void Main(string[] args)
         {
+            C c = new C();
+
+            // s1, s2, s5, s6, s7 
+            // c.s1(); //A::s1, A::v1, A::s3
+            //c.s2(); //A::s2, A::s1, A::v1, A::s3
+            //c.s5(); //B::s5, B::s3
+            //c.s6(); //C::s6, B::v1
+            //c.s7();//C::s7, B::v2,B::v1
+
+
             Console.ReadKey();
 
         }
